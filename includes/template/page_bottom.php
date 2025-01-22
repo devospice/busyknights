6ef811b2226921de7e8a57c2192d95c33541ec55
devospice <devospice@gmail.com>
2025-01-22 10:52:57 -0500

    cleared

--- includes/template/page_bottom.php
+++ includes/template/page_bottom.php
@@ -2,7 +2,9 @@
     	<div id="footer-logo">
     		<img src="/assets/images/logos/busy-knights-KO.png" >
         </div>
-        <p>A division of FIDIM Interactive, LLC.  &copy; 2018.  All rights reserved.  <span class="links"><a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a> |  <a href="#">Security Statement</a> |  <a href="#">Contact Us</a></span></p>
+        <p>A division of FIDIM Interactive, LLC.  &copy; <?php echo date("Y"); ?>.  All rights reserved.  <span class="links"><a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a> |  <a href="#">Security Statement</a> |  <a href="#">Contact Us</a></span>
+		
+		</p>
 	</footer>
 	
 	
@@ -11,6 +13,20 @@
 <?php
 	include("includes/template/overlays.php");
 ?>
+<!--<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<script>
+	
+	const TransactionsApp = {
+		data() {
+			return {
+				title: "Hello Vue world",
+				msg: "This is a test"
+			}
+		}
+	}
+	
+	Vue.createApp(TransactionsApp).mount("#container");
+</script>-->
 
 </body>
 </html>
